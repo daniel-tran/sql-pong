9434cff185fce6bc3b7eca8b53bb2b0584025bf8
Daniel Tran <daniel.tran11@gmail.com>
2022-11-11 19:41:19 +1100

    And so it begins
    This should contain a minimal setup to move the paddles, move the ball and increment scoring when the ball hits the left and right sides

---
+++ pong.sql
@@ -0,0 +1,200 @@
+-- Setup commands
+CREATE SCHEMA IF NOT EXISTS pong;
+
+DROP TABLE IF EXISTS pong.screen;
+CREATE TABLE IF NOT EXISTS pong.screen (rowNumber, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9) AS VALUES
+(1, '', '', '', '', '', '', '', '', ''),
+(2, '', '', '', '', '', '', '', '', ''),
+(3, '', '', '', '', '', '', '', '', ''),
+(4, '#', '', '', '', '', '', '', '', '#'),
+(5, '#', '', '', '', '@', '', '', '', '#'),
+(6, '#', '', '', '', '', '', '', '', '#'),
+(7, '', '', '', '', '', '', '', '', ''),
+(8, '', '', '', '', '', '', '', '', ''),
+(9, '', '', '', '', '', '', '', '', '');
+
+DROP TABLE IF EXISTS pong.players;
+CREATE TABLE IF NOT EXISTS pong.players (playerNumber, top, bottom) AS VALUES
+(1, 4, 6),
+(2, 4, 6);
+
+DROP TABLE IF EXISTS pong.ball;
+CREATE TABLE IF NOT EXISTS pong.ball (x, y, xDirection, yDirection) AS VALUES
+(5, 5, 1, -1);
+
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+
+-- Just a dummy function that should eventually update the player's location
+CREATE OR REPLACE FUNCTION pong.movePlayer(playerToMove integer, actionValue integer) RETURNS integer AS $$
+  DECLARE playerTop integer;
+  DECLARE playerTopNew integer;
+  DECLARE playerBottom integer;
+  DECLARE playerBottomNew integer;
+  DECLARE screenHeight integer;
+BEGIN
+  SELECT COUNT(*) INTO screenHeight FROM pong.screen;
+  -- Keep the player's paddle within the screen height
+  SELECT top INTO playerTop FROM pong.players WHERE playernumber = playerToMove;
+  SELECT CASE
+    WHEN actionValue > 0 AND bottom < screenHeight THEN top + 1
+	WHEN actionValue < 0 AND top > 1 THEN top - 1
+	ELSE top
+  END INTO playerTopNew FROM pong.players WHERE playernumber = playerToMove;
+  SELECT bottom INTO playerBottom FROM pong.players WHERE playernumber = playerToMove;
+  SELECT CASE
+    WHEN actionValue > 0 AND bottom < screenHeight THEN bottom + 1
+	WHEN actionValue < 0 AND top > 1 THEN bottom - 1
+	ELSE bottom
+  END INTO playerBottomNew FROM pong.players WHERE playernumber = playerToMove;
+  
+  -- Wipe out the paddle and redraw it again at its new coordinates
+  UPDATE pong.screen SET (cell1) = (
+	  SELECT ''
+  ) WHERE playerToMove = 1 AND pong.screen.rowNumber >= playerTop AND pong.screen.rowNumber <= playerBottom;
+  UPDATE pong.screen SET (cell1) = (
+	  SELECT '#'
+  ) WHERE playerToMove = 1 AND pong.screen.rowNumber >= playerTopNew AND pong.screen.rowNumber <= playerBottomNew;
+  -- Need to repeat the same UPDATE for the other player, since the field name can't be dynamically assigned
+  UPDATE pong.screen SET (cell9) = (
+	  SELECT ''
+  ) WHERE playerToMove = 2 AND pong.screen.rowNumber >= playerTop AND pong.screen.rowNumber <= playerBottom;
+  UPDATE pong.screen SET (cell9) = (
+	  SELECT '#'
+  ) WHERE playerToMove = 2 AND pong.screen.rowNumber >= playerTopNew AND pong.screen.rowNumber <= playerBottomNew;
+
+  UPDATE pong.players SET (top, bottom) = (playerTopNew, playerBottomNew) WHERE playernumber = playerToMove;
+  RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
+SELECT pong.movePlayer(1, -1);
+SELECT pong.movePlayer(2, 1);
+SELECT * from pong.screen ORDER BY rowNumber ASC;
+
+-- Just a dummy function that should eventually update the ball's location
+CREATE OR REPLACE FUNCTION pong.moveBall() RETURNS integer AS $$
+  DECLARE xNew integer;
+  DECLARE yNew integer;
+  DECLARE xDirectionNew integer;
+  DECLARE yDirectionNew integer;
+  DECLARE rowWithBall integer;
+  DECLARE screenHeight integer;
+  DECLARE screenWidth integer;
+  DECLARE top1 integer;
+  DECLARE bottom1 integer;
+  DECLARE top2 integer;
+  DECLARE bottom2 integer;
+  DECLARE whichPlayerHasScored integer;
+BEGIN
+  SELECT 9 INTO screenWidth;
+  SELECT COUNT(*) INTO screenHeight FROM pong.screen;
+  SELECT top INTO top1 FROM pong.players WHERE playernumber = 1;
+  SELECT bottom INTO bottom1 FROM pong.players WHERE playernumber = 1;
+  SELECT top INTO top2 FROM pong.players WHERE playernumber = 2;
+  SELECT bottom INTO bottom2 FROM pong.players WHERE playernumber = 2;
+  
+  -- Calculate the player index that scored based on where the ball will move next.
+  -- 0 = No player has scored in this current turn
+  SELECT CASE
+    WHEN (x + xDirection) <= 1 AND ((y + yDirection) < top1 OR (y + yDirection) > bottom1) THEN 2
+	WHEN (x + xDirection) >= screenWidth AND ((y + yDirection) < top2 OR (y + yDirection) > bottom2) THEN 1
+	ELSE 0
+  END INTO whichPlayerHasScored FROM pong.ball;
+  -- Register the score
+  UPDATE pong.players SET (score) = (SELECT score + 1) WHERE playernumber = whichPlayerHasScored;
+  
+  -- Keep the ball within the boundaries of the screen
+  SELECT CASE
+    -- Reset the X coordinate if the P1 paddle missed the hit
+    WHEN whichPlayerHasScored > 0 THEN 5
+    ELSE GREATEST(1, LEAST(screenWidth, x + xDirection))
+  END INTO xNew FROM pong.ball;
+  SELECT CASE
+    -- Reset the Y coordinate if the P1 paddle missed the hit
+    WHEN whichPlayerHasScored > 0 THEN 5
+    ELSE GREATEST(1, LEAST(screenHeight, y + yDirection))
+  END INTO yNew FROM pong.ball;
+  -- Reflect the ball when it hits the edge of the screen
+  SELECT CASE
+    WHEN xNew >= screenWidth OR (xNew = 1 AND y >= top1 AND y <= bottom1) THEN xDirection * -1
+	ELSE xDirection
+  END INTO xDirectionNew FROM pong.ball;
+  SELECT CASE
+    WHEN yNew <= 1 OR yNew >= screenHeight THEN yDirection * -1
+	ELSE yDirection
+  END INTO yDirectionNew FROM pong.ball;
+  
+  SELECT y INTO rowWithBall FROM pong.ball;
+  
+  -- Find the row with the ball and blindly remove it from all fields, since Postgres can't access fields by column number
+  UPDATE pong.screen SET (cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9) = (
+	  REPLACE(cell1, '@', ''),
+      REPLACE(cell2, '@', ''),
+      REPLACE(cell3, '@', ''),
+      REPLACE(cell4, '@', ''),
+      REPLACE(cell5, '@', ''),
+      REPLACE(cell6, '@', ''),
+      REPLACE(cell7, '@', ''),
+      REPLACE(cell8, '@', ''),
+      REPLACE(cell9, '@', '')
+  ) WHERE rowNumber = rowWithBall;
+  -- Redraw the ball based on its new coordinates
+  UPDATE pong.screen SET (cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9) = (
+	SELECT CASE
+      -- Draw the paddle touching the ball, or just the paddle if there's no hit detected
+	  WHEN yNew >= top1 AND yNew <= bottom1 THEN 
+	    CASE
+	      WHEN xNew = 1 THEN '#@'
+	      ELSE '#'
+	    END
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 2 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 3 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 4 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 5 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 6 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 7 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  WHEN xNew = 8 THEN '@'
+	  ELSE ''
+	END,
+	CASE
+	  -- Draw the paddle touching the ball, or just the paddle if there's no hit detected
+	  WHEN yNew >= top2 AND yNew <= bottom2 THEN 
+	    CASE
+	      WHEN xNew = screenWidth THEN '@#'
+	      ELSE '#'
+	    END
+	  ELSE ''
+	END
+  ) WHERE rowNumber = yNew;
+
+  UPDATE pong.ball SET (x, y, xDirection, yDirection) = (xNew, yNew, xDirectionNew, yDirectionNew);
+  RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
+-- Ball MUST move before the player, otherwise the paddle can't be drawn when hitting the paddle, since the paddle will draw over the ball's cell
+SELECT pong.movePlayer(2, 1);
+SELECT pong.moveBall();
+-- ORDER BY is necessary, since UPDATE won't preserve the original row order by default
+SELECT * from pong.screen ORDER BY rowNumber ASC;
